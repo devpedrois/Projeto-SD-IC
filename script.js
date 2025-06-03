2c50616f424dfcab73f1174448904bb694db4e10
Dio <diogoswc@gmail.com>
2025-06-03 18:56:31 -0300

    Ajustes finais

--- script.js
+++ script.js
@@ -6,3 +6,22 @@
     sidebar.classList.toggle("expandido");
   };
 
+  document.getElementById("download-btn").addEventListener("click", function download() {
+      const link = document.createElement("a");
+      link.href = "https://drive.google.com/file/d/1o6fTtGe7TerOn6wKzoirMDVpbqZCZQLG/view?usp=sharing";
+      link.download = "Coldwheels.ino";                      
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+  });
+  document.getElementsById("montagem-btn").addEventListener("click", function montagempdf() {
+      const link = document.createElement("a");
+      link.href = "https://drive.google.com/file/d/1OGVx9FjG8Hi16L22csLt_G88dVNNyEkj/view?usp=sharing";
+      link.download = "MODELAGEM PRATICA GRUPO-KIT 08.pdf";                      
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+  });
+ 
+
+
