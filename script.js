dce792d79aed2c38aa78a8b96dc7d6bdb246294f
Pedrois <pedeoiranildo@gmail.com>
2025-06-03 12:25:29 -0300

    Ajeitando identação

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
 
-  const menuBtn = document.getElementById("menuBtn");
-  const sidebar = document.getElementById("sidebar");
-
-  menuBtn.onclick = function () {
-    sidebar.classList.toggle("expandido");
-  };
-
+menuBtn.onclick = function () {
+  sidebar.classList.toggle("expandido");
+};
