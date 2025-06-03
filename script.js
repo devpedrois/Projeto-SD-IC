2e7523ad0cad87872ddb6c76358f39f075aaac30
Nicolasnat <nicolas.natario0@gmail.com>
2025-06-02 22:29:17 -0300

    Criei a base do css e a pagina principal

---
+++ script.js
@@ -0,0 +1,8 @@
+
+  const menuBtn = document.getElementById("menuBtn");
+  const sidebar = document.getElementById("sidebar");
+
+  menuBtn.onclick = function () {
+    sidebar.classList.toggle("expandido");
+  };
+
